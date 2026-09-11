36835943d2c3539695d7a291b614e94886075ce5
unknown <supratim186@gmail.com>
2026-09-11 19:26:11 +0530

    Fixed project errors

--- backend/app/main_backup.py
+++ backend/app/main_backup.py
@@ -30,7 +30,8 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:3000",
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "https://ai-landslide-monitoring-1-boy7.onrender.com/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
